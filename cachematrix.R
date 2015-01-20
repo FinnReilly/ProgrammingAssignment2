baf3ec681fd993704f30bb42142903e4817c712e
Finn Reilly <finnreilly1984@gmail.com>
2015-01-20 23:34:58 +0000

    Second programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##example use:
+##test<-makeCacheMatrix()
+##test$set(replicate(2,rnorm(2)))     -This is a nice way to create a random matrix.
+##cacheSolve(test)                    -This will calculate the inverse - run twice to 
+##                                     see different behaviour when already cached.
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## Works almost like a Class in C# with its own methods, must be constructed before it can be used
+## caches the input matrix with set, all methods relate to setting or retrieval of data only
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() x
+  setinv<-function(data) inv<<-(data)
+  getinv<-function() inv
+  list(                                
+    set=set,                           
+    get=get,
+    setinv=setinv,
+    getinv=getinv
+  )
 }
 
-
-## Write a short comment describing this function
+#cacheSolve
+#takes an 'object instance' of the above function and accesses its methods, as in OOP
+#returns an inverse of the previously cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if(!is.null(inv)){
+    message("Already in cache!")
+    return(inv)
+  }
+  message("Calculating...")  #included this message so you can tell whether cached 
+  input<-x$get()             #inverted matrix has been calculated yet
+  
+  x$setinv(solve(input))
+  x$getinv()
 }
